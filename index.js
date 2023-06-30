ffb5e6e65dbaaa2750405d7c09b45cf7ea069462
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-06-30 20:05:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  word.toLowerCase
+  reversed_word = word.split("").reverse().join("")
+  if(word ===reversed_word ){
+    return true
+  }else{
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  initialize isPandilome function
+  convert the string to lower case
+  reverse the string
+  compare the string with the reversed string
+  if equal
+  return true
+  else return false
 */
 
 /*
   Add written explanation of your solution here
+  i should write an usPandilome function which returns true if the string passed fowards is the same as backwards and should else it should return false
 */
 
 // You can run `node index.js` to view these console logs
